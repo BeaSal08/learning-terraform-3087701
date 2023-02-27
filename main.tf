6e23acfc3a7d58d84a5ee2fa03262d762c6c6785
BeaSal08 <105531396+BeaSal08@users.noreply.github.com>
2023-02-27 15:17:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+data "aws_ami" "amazon_ami" {
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.20220606.1-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  most_recent = true
+  owners      = ["amazon"]
+}
+
 resource "aws_instance" "web" {
-  ami           = ami-03c3fde85049dd22b
+  ami = data.aws_ami.amazon_ami.id
   instance_type = "t2.micro"
   availability_zone = "us-east-1e"
 
