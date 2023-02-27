e54b69f2ace8574722503cb187e54136c613d59b
BeaSal08 <105531396+BeaSal08@users.noreply.github.com>
2023-02-27 15:25:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
-  region = "us-east-1"
 
   tags = {
     Name = "HelloWorld"
