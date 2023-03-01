7d95253373f2e04a624dddef44d640cceb56a5b0
BeaSal08 <105531396+BeaSal08@users.noreply.github.com>
2023-03-01 10:45:46 +0800

    add output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
-#
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "environment_url" {
+  value = module.alb.lb_dns_name
+}
+
+output "alb_name" {
+    value = module.alb.lb_arn
+}
